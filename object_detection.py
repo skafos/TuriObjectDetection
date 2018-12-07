c31f10b7efc8ab3636b284655f0cc61204b2ac4c
griffinwalkerMM <griffin@metismachine.com>
2018-12-07 12:53:18 -0500

    Add comments to Skafos job and fix notebooks

--- object_detection.py
+++ object_detection.py
@@ -15,7 +15,7 @@ data_path = "ig02.sframe.tar.gz"
 #ska.log("Retrieving the images from online", labels = ['image_similarity'])
 retrieve = urllib.request.urlretrieve(data_url, data_path)
 
-# extract the file
+## pull the compressed data and extract it
 #ska.log("Images downloaded, extracting the images", labels = ['image_similarity'])
 tar = tarfile.open(data_path)
 tar.extractall()
@@ -27,8 +27,8 @@ data =  tc.SFrame('ig02.sframe')
 # Make a train-test split
 train_data, test_data = data.random_split(0.8)
 
-# Create a model
-model = tc.object_detector.create(train_data, max_iterations=900)
+# Train an object detection model. This will take approximately 1.5 days on Skafos with 6CPU's and 10GB of RAM.
+model = tc.object_detector.create(train_data)
 
 # Save the model for later use in Skafos
 coreml_model_name = 'object_detection.mlmodel'
